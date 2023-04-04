0b408375cb61486e9f344a747d389ce8f5f44a80
ellin45 <119480528+ellin45@users.noreply.github.com>
2023-04-04 23:59:52 +0900

    second commit

--- word.js
+++ word.js
@@ -1,53 +1,45 @@
-let cnt =0; // 클릭 횟수 초기화
-const maxClicks =10; // 최대 클릭 가능 횟수
+let cnt = 0; // 클릭 횟수 초기화
+const maxClicks = 10; // 최대 클릭 가능 횟수
 
 //id가 'word'인 버튼 클릭
+document.getElementById('word').addEventListener('click', function () {
+  cnt++;
+  if (cnt > maxClicks) {
+    document.getElementById('word').removeEventListener('click', this); // 이벤트 제거
+    return; // 함수 종료
+  }
 
-document.getElementById('word').addEventListener('click', function(){
-    cnt++;
-    if(cnt > maxClicks) { // 최대 클릭 가능 횟수를 초과하면
-        removeEventListener // 이벤트 제거
-        return; // 함수 종료
-    }
-
-    //JSON 파일에서 키 불러오기
-    const json = {"key" : "value"};
-    const data = JSON.parse(json);
-    const key = data.key;
-
-    //key 값을 박스에 노출
-    const box = document.getElementById('box');
-    box.textContent = key;
-    box.style.display = 'block';
-
-    //hide버튼 클릭시 박스를 숨기도록 설정
-    const hideButton = document.getElementById('hide');
-    hideButton.addEventListener('click',function() {
-        box.style.display = 'none';
+  //JSON 파일에서 키 불러오기
+  fetch('english_word.json')
+    .then(response => response.json())
+    .then(data => {
+      const keys = Object.keys(data);
+      const firstKey = keys[0];
+      //key값을 박스에 노출
+      const box = document.getElementById('box1');
+      box.textContent = firstKey;
+      box.style.display = 'block';
     });
 });
 
-    document.getElementById('mean').addEventListener('click',function() {
-        cnt++; 
-        if(cnt > maxClicks) {
-            removeEventListener();
-            return;
-        }
-        
-        //JSON파일에서 값 불러오기
-        const json = {"key": "value"};
-        const data = JSON.parse(json);
-        const value = data.value;
+//id가 'mean'인 버튼 클릭
+document.getElementById('mean').addEventListener('click', function () {
+  cnt++;
+  if (cnt > maxClicks) {
+    document.getElementById('mean').removeEventListener('click', this);
+    return;
+  }
 
+  //Ajax를 이용해 JSON파일에서 데이터 불러오기
+  fetch('english_word.json')
+    .then(response => response.json())
+    .then(data => {
+      for (let key in data) {
         //value값을 박스에 노출
-        const box = document.getElementById('box');
-        box.textContent = value;
+        const box = document.getElementById('box2');
+        box.textContent = data[key];
         box.style.display = 'block';
-
-        //'hide' 버튼 클릭 시 박스가 숨기도록 설정
-        const hideButton = document.getElementById('hide');
-        hideButton.addEventListener('click', function(
-            box.style.display = 'none';
-        ));
+        break;
+      }
     });
-
+});
